76da9bb963a40f5b0835aabfd7422e9d5ef5bdca
NadirFernanda <fernandadapiedade2018gmail.com>
2026-02-08 18:25:13 +0100

    Add user menu link to change password in header

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -1,3 +1,17 @@
 <header>
     <h1>Sistema de Gestão</h1>
+
+    @auth
+    <div class="user-menu" style="float:right">
+        <span>{{ auth()->user()->email }}</span>
+        &nbsp;|&nbsp;
+        <a href="{{ route('password.change') }}">Alterar senha</a>
+        &nbsp;|&nbsp;
+        <form method="POST" action="{{ route('logout') }}" style="display:inline">
+            @csrf
+            <button type="submit" style="background:none;border:none;color:inherit;cursor:pointer;padding:0">Sair</button>
+        </form>
+    </div>
+    @endauth
+
 </header>
