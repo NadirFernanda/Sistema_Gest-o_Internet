e6c84d14b869dddaa7bf3c2a23bde0b2043549c6
NadirFernanda <fernandadapiedade2018gmail.com>
2026-02-08 22:18:27 +0100

    Remove email and role from header

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -3,16 +3,6 @@
 
     @auth
     <div class="user-menu">
-        <div class="user-info">
-            <span class="user-email">{{ auth()->user()->email }}</span>
-            @php
-                $activeRole = session('acting_as_role') ?: (auth()->user()->getRoleNames()->first() ?? null);
-            @endphp
-            @if($activeRole)
-                <span class="role-active">Role: <span class="role-badge">{{ $activeRole }}</span></span>
-            @endif
-        </div>
-
         <div class="user-actions">
             <a href="{{ route('password.change') }}">Alterar senha</a>
             <form method="POST" action="{{ route('logout') }}" style="display:inline">
