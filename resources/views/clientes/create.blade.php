167abf8d057122d4dab570ba854af61faa9ccc39
NadirFernanda <fernandadapiedade2018gmail.com>
2026-02-11 20:44:57 +0100

    style: add back-arrow icon to 'Voltar' button in clientes create

--- resources/views/clientes/create.blade.php
+++ resources/views/clientes/create.blade.php
@@ -3,7 +3,12 @@
 @section('content')
 
 <div class="container">
-    <a href="{{ route('clientes') }}" class="btn btn-secondary mb-3">Voltar</a>
+    <a href="{{ route('clientes') }}" class="btn btn-secondary mb-3">
+        <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" style="vertical-align:middle;margin-right:8px;">
+            <path d="M15 18l-6-6 6-6" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+        </svg>
+        Voltar
+    </a>
     <div class="client-card">
         <header class="client-card-header">
             <h2>Cadastrar Cliente</h2>
