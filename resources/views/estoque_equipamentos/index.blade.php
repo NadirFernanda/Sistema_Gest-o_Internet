44ba822fdabe1d9e0f00611f1d443a15bc054b0e
NadirFernanda <fernandadapiedade2018gmail.com>
2026-02-14 22:58:15 +0100

    fix(estoque): coluna Marca exibe o campo nome do equipamento (valor do formulário)

--- resources/views/estoque_equipamentos/index.blade.php
+++ resources/views/estoque_equipamentos/index.blade.php
@@ -108,7 +108,7 @@
             <tbody>
                 @forelse($equipamentos as $equipamento)
                     <tr>
-                        <td style="text-align:center;vertical-align:middle;">{{ $equipamento->marca }}</td>
+                        <td style="text-align:center;vertical-align:middle;">{{ $equipamento->nome }}</td>
                         <td style="text-align:center;vertical-align:middle;">{{ $equipamento->descricao }}</td>
                         <td style="text-align:center;vertical-align:middle;">{{ $equipamento->modelo }}</td>
                         <td style="text-align:center;vertical-align:middle;">{{ $equipamento->numero_serie }}</td>
