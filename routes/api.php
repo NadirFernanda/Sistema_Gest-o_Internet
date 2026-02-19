fff6f07e7e80bd88d7cb175a55b375bd1dbafcf4
NadirFernanda <fernandadapiedade2018gmail.com>
2026-02-19 23:21:12 +0100

    Auto-commit: push requested by user

--- routes/api.php
+++ routes/api.php
@@ -13,10 +13,12 @@ Route::get('/planos/{id}', [PlanoController::class, 'show']);
 Route::put('/planos/{id}', [PlanoController::class, 'update']);
 Route::delete('/planos/{id}', [PlanoController::class, 'destroy']);
 Route::post('/login', [AuthController::class, 'login']);
-Route::post('/clientes', [ClienteController::class, 'store']);
-// Clients listing: protect with VerifyApiToken middleware + rate limiting.
+// Clients endpoints: protect with VerifyApiToken middleware + rate limiting.
 // Use fully-qualified class name to avoid relying on route middleware alias resolution.
-Route::middleware([\App\Http\Middleware\VerifyApiToken::class, 'throttle:60,1'])->get('/clientes', [ClienteController::class, 'index']);
-Route::put('/clientes/{id}', [ClienteController::class, 'update']);
-Route::delete('/clientes/{id}', [ClienteController::class, 'destroy']);
+Route::middleware([\App\Http\Middleware\VerifyApiToken::class, 'throttle:60,1'])->group(function () {
+	Route::post('/clientes', [ClienteController::class, 'store']);
+	Route::get('/clientes', [ClienteController::class, 'index']);
+	Route::put('/clientes/{id}', [ClienteController::class, 'update']);
+	Route::delete('/clientes/{id}', [ClienteController::class, 'destroy']);
+});
 Route::post('/alertas/disparar', [ClienteController::class, 'dispararAlertas']);
