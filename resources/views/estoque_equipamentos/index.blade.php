ffa37d0a31115a73380f646223946a740020226a
NadirFernanda <fernandadapiedade2018gmail.com>
2026-02-19 22:42:50 +0100

    Temporarily show 'Cadastrar' button in estoque_equipamentos view

--- resources/views/estoque_equipamentos/index.blade.php
+++ resources/views/estoque_equipamentos/index.blade.php
@@ -21,9 +21,8 @@
                 @endif
             </form>
             <div style="display:flex;gap:8px;">
-                @can('estoque_equipamentos.create')
+                <!-- Temporarily show the button without permission check -->
                 <a href="{{ route('estoque_equipamentos.create') }}" class="btn btn-cta">Cadastrar</a>
-                @endcan
                 
                 <a href="{{ route('dashboard') }}" class="btn btn-ghost">Painel</a>
             </div>
