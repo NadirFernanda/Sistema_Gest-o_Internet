3be42081378511ee61dd2c81f82ad5555393191b
NadirFernanda <fernandadapiedade2018gmail.com>
2026-02-24 00:34:14 +0100

    Add favicon tags to header partial

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -1,5 +1,12 @@
 <header>
 
+    <!-- Favicons -->
+    <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png">
+    <link rel="manifest" href="/favicons/site.webmanifest">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="theme-color" content="#ffffff">
 
     @auth
     {{-- user actions removed (Alterar senha / Sair) --}}
