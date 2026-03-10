16dee5888feb11ab88e162777302d0f2720fe269
NadirFernanda <fernandadapiedade2018gmail.com>
2026-03-10 04:28:45 +0100

    perf: load Google Fonts async (non-blocking) to eliminate FOUC

--- loja/resources/views/layouts/app.blade.php
+++ loja/resources/views/layouts/app.blade.php
@@ -6,7 +6,9 @@
     <title>{{ config('app.name', 'Loja') }}</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800;900&display=swap&font-display=swap" rel="stylesheet">
+    {{-- Non-blocking font load: page renders immediately; Inter applies when ready --}}
+    <link rel="preload" href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800;900&display=swap" as="style" onload="this.onload=null;this.rel='stylesheet'">
+    <noscript><link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800;900&display=swap"></noscript>
     {{-- Carregar CSS de forma mutuamente exclusiva: Vite quando disponivel, fallback caso contrario. --}}
     @if (file_exists(public_path('build/manifest.json')))
       @vite(['resources/css/app.css','resources/js/app.js'])
