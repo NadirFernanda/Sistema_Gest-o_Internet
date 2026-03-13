91d9ce75db862e881ae6b358521b37c89a6d4ef4
NadirFernanda <fernandadapiedade2018gmail.com>
2026-03-13 01:17:04 +0100

    Fix: contraste do texto 'a partir de' no card institucional (amarelo de destaque)

--- loja/resources/js/app.js
+++ loja/resources/js/app.js
@@ -15,7 +15,7 @@ function renderFamilyCard(plan) {
 	var featuredClass = plan.is_popular ? ' plan-card--featured' : '';
 	var body = '';
 	if (plan.preco) {
-		var prefix = tipo.indexOf('institucional') !== -1 ? '<span style="font-size:0.7em;font-weight:400;display:block;margin-bottom:0.1em;">a partir de</span>' : '';
+		var prefix = tipo.indexOf('institucional') !== -1 ? '<span style="font-size:0.7em;font-weight:400;display:block;margin-bottom:0.1em;color:#f9d23c;">a partir de</span>' : '';
 		body += '<div class="plan-price-row">' + prefix + '<span class="plan-price">'
 			+ Number(plan.preco).toLocaleString('pt-PT')
 			+ '</span><span class="plan-currency">Kz</span></div>';
