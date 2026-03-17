9f8fd73fa6c99e9704fce60dc833e6898c64dd92
NadirFernanda <fernandadapiedade2018gmail.com>
2026-03-17 01:31:39 +0100

    Fix: Voltar button width on planos create page

--- resources/views/planos/create.blade.php
+++ resources/views/planos/create.blade.php
@@ -8,7 +8,7 @@
     <div class="planos-container">
         {{-- Página sem header: formulário isolado --}}
         <div style="max-width:1100px; margin:12px auto 0; text-align:center;">
-            <a href="{{ route('planos.index') }}" class="btn btn-ghost" title="Voltar" aria-label="Voltar" style="display:inline-block; margin:0 auto 12px;">Voltar</a>
+            <a href="{{ route('planos.index') }}" class="btn btn-ghost" title="Voltar" aria-label="Voltar" style="display:inline-block; width:auto; margin:0 auto 12px;">Voltar</a>
 
             @include('planos._form')
         </div>
